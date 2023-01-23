141cd8daf175f49599246d30445b49eefb638d81
abstractvector <abstractvector@users.noreply.github.com>
2023-01-23 10:52:26 +0100

    Add unit tests for utils

---
+++ __tests__/utils.test.ts
@@ -0,0 +1,39 @@
+import { attributeArrayToReactProps } from '../src/utils';
+
+describe('attributeArrayToReactProps function', () => {
+  test('Returns an empty object by default', () => {
+    expect(attributeArrayToReactProps()).toEqual({});
+  });
+
+  test('Returns an empty object for an empty input', () => {
+    expect(attributeArrayToReactProps([])).toEqual({});
+  });
+
+  test('Returns an object with one entry', () => {
+    expect(attributeArrayToReactProps(['key', 'value'])).toEqual({ key: 'value' });
+  });
+
+  test('Returns an object with one entry when only given a key', () => {
+    expect(attributeArrayToReactProps(['key'])).toEqual({ key: '' });
+  });
+
+  test('Returns an object with one empty entry when given an odd number of inputs', () => {
+    expect(attributeArrayToReactProps(['key1', 'value1', 'key2'])).toEqual({ key1: 'value1', key2: '' });
+  });
+
+  test('Returns an object of keys from an array of inputs', () => {
+    expect(attributeArrayToReactProps(['key1', 'value1', 'key2', 'value2', 'key3', 'value3'])).toEqual({
+      key1: 'value1',
+      key2: 'value2',
+      key3: 'value3',
+    });
+  });
+
+  test('Returns an object containing string values for non-string-looking inputs', () => {
+    expect(attributeArrayToReactProps(['string', 'string', 'number', '1', 'boolean', 'true'])).toEqual({
+      string: 'string',
+      number: '1',
+      boolean: 'true',
+    });
+  });
+});
--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,9 @@
-export function attributeArrayToReactProps(attributeArray: string[] = []): Record<string, unknown> {
-  const attributes: Record<string, unknown> = {};
+export function attributeArrayToReactProps(attributeArray: string[] = []): Record<string, string> {
+  const attributes: Record<string, string> = {};
 
   for (let i = 0; i < attributeArray.length; i += 2) {
     const a = attributeArray[i];
-    if (a !== undefined) attributes[a] = attributeArray[i + 1];
+    if (a !== undefined) attributes[a] = attributeArray[i + 1] ?? '';
   }
 
   return attributes;
